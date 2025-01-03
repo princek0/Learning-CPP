9f47d0ce76fab7e69c63158ada2bbd1d9c39ee82
Prince <62904535+princek0@users.noreply.github.com>
2025-01-03 17:36:28 +0000

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Preprocessor Directive, this line will be processed before compliation.
+
+int main() // The name of a function is called an 'identifier'
+{
+   std::cout << "Hello world!"; // Statements must end with a ; 
+   return 0; // Returning 0 indicates that the function worked properly 
+}
