6550448ddbcbf9add05c9e8e95857624c14a0ac2
Prince <yuviyukt@googlemail.com>
2025-04-23 12:26:48 +0100

    unfinished

---
+++ Problems/USACO/Bronze/Blocks Feb22.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char block1[7];
+    char block2[7];
+    char block3[7];
+    char block4[7];
+    int n;
+
+    cin >> n;
+    cin >> block1;
+    cin >> block2;
+    cin >> block3;
+    cin >> block4;
+
+    for (int i = 1; i<=n; i++){
+        
+    }
+    return 0;
+}
+
+// https://usaco.org/index.php?page=viewproblem2&cpid=1205
+
+
+
