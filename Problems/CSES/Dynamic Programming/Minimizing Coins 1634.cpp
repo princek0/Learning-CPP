c9837c04e78bb24a7440adcbcd6a59339372b3e8
Prince <yuviyukt@googlemail.com>
2025-05-21 15:45:23 +0100

    Update Minimizing Coins 1634.cpp

--- Problems/CSES/Dynamic Programming/Minimizing Coins 1634.cpp
+++ Problems/CSES/Dynamic Programming/Minimizing Coins 1634.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
-}
+const int INF = 1e9;
+vector<int> answers;
+vector<int> coins; // list of coin values
 
 int main(){
     int n; // number of coins
     int x; // desired number
-    vector<int> coins; // list of coin values
 
     cin >> n;
     cin >> x;
@@ -18,10 +20,29 @@ int main(){
         coins.push_back(coin);
     }
 
+    answers.resize(x+1);
+    answers[0] = 0;
 
+    for (int i = 1; i<x+1; i++){
+        int result = INF;
+        for (int j = 0; j < (int) coins.size(); j++){
 
+            if (i - coins[j] < 0){
+                continue;
+            }
 
+            if (answers[i - coins[j]]+1 < result){
+                result = answers[i - coins[j]]+1;
+            }
+        }
+        answers[i] = result;
+    }
 
+    if (answers[x] >= INF){
+        cout << -1;
+    } else{
+        cout << answers[x];
+    }
 
     return 0;
 }
